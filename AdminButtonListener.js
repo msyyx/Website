c5b6d686449ff9b59cf2463e0fbbbfe6f27e2e56
zihang <fuzihang1995@gmail.com>
2016-03-08 23:36:45 +0800

    prototype of Admin page

---
+++ AdminButtonListener.js
@@ -0,0 +1,15 @@
+$('#security').on('click', function(e){
+  //$('#content').html('');
+  //$('.form').hide();
+  $('#password-sheet').show();
+})
+
+$('#info').on('click', function(e){
+  //$('#content').html('');
+  //$('.form').hide();
+  $('#info-sheet').show();
+})
+
+$('#back').on('click', function(e) {
+  $('.form').hide();
+})
