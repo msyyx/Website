228c6a0bbccd2ea1482cee1f49fec7754170e232
Jiaqin Meng <rain950617@gmail.com>
2016-03-31 22:21:16 -0400

    Revert "add profile route to server"
    This reverts commit b57aa0b0c6b06d5d2015d9e83ee53d743cd347e4.

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 
 var hostRoute = require('./controllers/routeHost.js');
 var userRoute = require('./controllers/routeUser.js');
-var profileRoute =  require('./controllers/routeProfile.js');
 
 var app = express();
 var port = 3000;
@@ -31,7 +30,6 @@ app.use(express.static(path.join(__dirname, 'view')));
 /** Routing Modules Configuration **/
 app.use('/host',hostRoute);
 app.use('/user',userRoute);
-app.use('/',profileRoute);
 app.use('/', function(req, res){res.sendFile('main.html', {root: "./view/"});});
 
 
