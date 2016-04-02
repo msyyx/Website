5e5017160e869463154e09fe922111cd7b9e0fb4
Jiaqin Meng <rain950617@gmail.com>
2016-04-02 01:44:14 -0400

    profile db update done

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var UserSchema = mongoose.Schema({
   username: String,
   email: String,
   password: String,
-  dateofbirth: {type: Date, default: Date.now},
+  dateofbirth: {type: String, default: ''},
   name: {type :String, default:''},
   recentlyvisit:{type :String, default:''},
 });
