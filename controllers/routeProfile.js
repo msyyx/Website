1280c55029310f7174e2fcc49f1951a24760c316
Jiaqin Meng <rain950617@gmail.com>
2016-04-01 20:55:02 -0400

    update profile function .add few attribute into user db schema

--- controllers/routeProfile.js
+++ controllers/routeProfile.js
@@ -12,4 +12,26 @@ router.get('/', function(req, res, next) {
     res.sendFile('profile.html', {root: "view/"});
 });
 
+router.put('/:id', function(req, res, next) {
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+        if (err) return next(err);
+       	User.update(
+       	{username: req.body.Username},
+       	{
+       		username: req.body.Username,
+       		email :req.body.email,
+       		dateofbirth: req.body.birthdate,
+       		name :req.body.name,
+
+       	},
+       	{upsert:true}
+       	)
+
+        res.json(user);
+
+
+        res.redirect( '/' );
+    });
+});
+
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -4,6 +4,9 @@ var UserSchema = mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  dateofbirth: {type: Date, default: Date.now},
+  name: {type :String, default:''},
+  recentlyvisit:{type :String, default:''},
 });
 
 var User = mongoose.model('User', UserSchema);
