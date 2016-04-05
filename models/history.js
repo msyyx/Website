8c58c7e2f2a70090dde8db66bcacb735b52fcd4a
zihang <fuzihang1995@gmail.com>
2016-04-05 19:55:15 +0800

    added history.js

---
+++ models/history.js
@@ -0,0 +1,20 @@
+/*to do list:
+ *EAT!
+ *Write the db, connect the db
+ *work on tokens
+ */
+
+ 'use strict';
+
+ var mongoose = require( 'mongoose' );
+ var Schema   = mongoose.Schema;
+
+ var historySchema = new Schema ({
+   hostName: {type: String, default: ''},
+   userName: {type: String, default: ''},
+   date: {type: String, default: ''},
+   orderDetail: {type: String, default: ''},
+   comment: {type: String, default: ''}
+ });
+
+ module.exports = mongoose.model('History', historySchema );
